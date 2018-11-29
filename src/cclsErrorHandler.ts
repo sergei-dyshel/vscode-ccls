a24db1506bbc6e197df5b59857397dc016ee483a
Sergei Dyshel <qyron.private@gmail.com>
2018-11-29 11:39:14 +0200

    Show progress in status bar

--- src/cclsErrorHandler.ts
+++ src/cclsErrorHandler.ts
@@ -1,9 +1,10 @@
-import {window, WorkspaceConfiguration} from 'vscode';
+import {StatusBarItem, window, WorkspaceConfiguration} from 'vscode';
 import {Message} from 'vscode-jsonrpc';
 import {CloseAction, ErrorAction, ErrorHandler} from 'vscode-languageclient';
 
 export class CclsErrorHandler implements ErrorHandler {
-  constructor(readonly config: WorkspaceConfiguration) {}
+  constructor(
+      readonly config: WorkspaceConfiguration, readonly status: StatusBarItem) {}
 
   public error(error: Error, message: Message, count: number): ErrorAction {
     return ErrorAction.Continue;
@@ -12,6 +13,9 @@ export class CclsErrorHandler implements ErrorHandler {
   public closed(): CloseAction {
     const notifyOnCrash = this.config.get('launch.notifyOnCrash');
     const restart = this.config.get('launch.autoRestart');
+
+    this.status.text = 'ccls: crashed';
+    this.status.color = 'red';
 
     if (notifyOnCrash) {
       window.showInformationMessage(
